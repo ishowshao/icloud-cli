348583688f098d6fe0fa5ce2db369701aa819e8c
ishowshao <ishowshao@me.com>
2021-11-15 22:13:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { program } = require('commander');
 program.version('1.0.0');
 
 program
+    .option('-s, --save', '存储: 文件名为 ${name}.${md5}.${ext}')
     .option('-a, --archive', '归档存储: 文件名为 ${md5}.${ext}')
-    .option('-s, --save', '存储: 文件名为 ${name}.${md5}.${ext}');
+    .option('-u, --uuid', '归档存储: 文件名为 ${uuid}.${ext}');
 
 program.parse(process.argv);
